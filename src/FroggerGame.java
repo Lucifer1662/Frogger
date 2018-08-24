67cbdaecd9aecd77faa168850200451ed4c4b092
Lucifer1662 <l.hawk99@hotmail.com>
2018-08-24 17:41:42 +1000

    Adding slick2d libraries

--- src/FroggerGame.java
+++ src/FroggerGame.java
@@ -1,9 +1,37 @@
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
 
-public class FroggerGame {
+public class FroggerGame extends BasicGame {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+
+	public FroggerGame(String title) {
+		super(title);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	@Override
+	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init(GameContainer arg0) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(GameContainer arg0, int arg1) throws SlickException {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
